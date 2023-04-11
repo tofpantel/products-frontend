c26f15cf017e2782669f9e36341dd9187410e178
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2023-04-11 20:27:11 +0300

    user-list

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { UserService } from './user.service';
 
 import { UsersListComponent } from './users-list/users-list.component';
 
@@ -8,6 +11,7 @@ const routes: Routes = [{ path: 'list', component: UsersListComponent }];
 
 @NgModule({
   declarations: [UsersListComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), HttpClientModule],
+  providers: [UserService],
 })
 export class UserModule {}
